28070b7d056c0c9d207f36560c16566d4a5a3e3c
Chethan L <chethan.shantha@gmail.com>
2023-01-21 13:32:56 +0000

    added prog1

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # C++ Programs
+
+<b>[Program 1](src/prog1.cpp)</b> : To find the quotient and reminder for two numbers after divition.
---
+++ src/prog1.cpp
@@ -0,0 +1,12 @@
+//prog1.cpp
+//find quotient and reminder.
+#include <iostream>
+using namespace std;
+
+int main(){
+	cout<<"Enter the two numbers:";
+	int x,y;
+	cin>>x>>y;
+	cout<<"The quotient = "<<(x/y)<<"\nThe reminder = "<<(x%y)<<endl;
+	return 0;
+}
